54bd14a827cbde8aaf1b6d35ee056dc5ebb2b766
daveying <daveying@163.com>
2017-04-21 14:26:19 +0800

    reinstall express, body-paser, fs, and markdown-it on ubuntu

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/parseMdStr', urlencodedParser, function (req, res) {
     }
 })
 
-var server = app.listen(80, function () {
+var server = app.listen(8880, function () {
     var host = server.address().address;
     var port = server.address().port;
 
